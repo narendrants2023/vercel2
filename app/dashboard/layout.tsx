e1c1c6cdcac34f75846ab1fd7e995258fea1a007
narendrants2023 <narendrants21@gmail.com>
2024-12-28 10:21:28 +0530

    initial1

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+// Dashboards have some sort of navigation that is shared across multiple pages. In Next.js, you can use a special layout.tsx file to create UI that is shared between multiple pages. Let's create a layout for the dashboard pages!
+
+// Inside the /dashboard folder, add a new file called layout.tsx and paste the following code:
+
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function Layout({children}: {children: React.ReactNode}){
+    return(
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+                <SideNav/>
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </div>
+        </div>
+    );
+}
